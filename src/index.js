43d260be6c711d79ff12d42722a16b72cd562d5b
Wonjin Cho <cwonjin1@gmail.com>
2021-10-11 11:03:45 -0400

    first solution added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function destroyer(arr) {
+    // console.log(arguments)
+    const argumentsArray = [...arguments].slice(1)
+    // [2,3]
+    // console.log(argumentsArray.slice(1), "<== sliced")
+    // console.log(argumentsArray, "argumentsArray")
+
+    const filteredArray = [];
+    for(let i=0; i<arr.length; i++){
+      if (!argumentsArray.includes(arr[i])){
+        filteredArray.push(arr[i])
+      }
+    }
+
+    return filteredArray;
+  }
+  
+  console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+  console.log(destroyer([1, 2, 3, 1, 2, 3],1, 2, 3));

dc8242e470bec0568b2515ae14c8803d64a2a546
Wonjin Cho <cwonjin1@gmail.com>
2021-10-12 21:02:20 -0400

    slice test

--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ function destroyer(arr) {
   
   console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
   console.log(destroyer([1, 2, 3, 1, 2, 3],1, 2, 3));
+  console.log(destroyer([1, 2, 3, 1, 2, 3],1, 2, 4));
